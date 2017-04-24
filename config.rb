78b58f8b531d4e5b995570add1efa199516a9fb8
David <david@aske.ws>
2017-04-24 18:31:39 -0400

    misc refactoring for config

--- config.rb
+++ config.rb
@@ -2,7 +2,36 @@
 require 'yaml'
 
 class Config
+
+  attr_reader :path, :todo_file
+
   def initialize(path)
-    puts path 
+    @path = path
+    @valid = false
+    begin
+      config = YAML.load(File.open(path))
+      if config.include? 'user'
+        name = config['user']['name'] if config['user'].include? 'name'
+      end
+
+      if config.include? 'data'
+        base_dir = config['data']['todo_base_dir'] if config['data'].include? 'todo_base_dir'
+        if base_dir 
+          @todo_file = base_dir + config['data']['todo_file'] if config['data'].include? 'todo_file'
+        end
+      end
+      
+      @valid = true
+    rescue ArgumentError => e
+      puts "Could not parse YAML: #{e.message}"
+    rescue Exception => e
+      puts "Unable to load config from: #{path}" 
+      puts "Exception : #{e.message}"
+    end
   end 
+
+  def valid?
+    @valid 
+  end
+
 end
