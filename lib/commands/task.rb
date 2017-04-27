5238c7eb34761d2d6c231b8f856bea73140f1a08
David <david@aske.ws>
2017-04-27 06:03:52 -0400

    task removal basically works

--- lib/commands/task.rb
+++ lib/commands/task.rb
@@ -8,7 +8,10 @@ module Askew
         list = Todo::List.new Startup.config.todo_file
         #puts list.by_priority "A"
         #puts list.sort_by { |task| task.priority }
-        puts list.sort_by {|item| [item.priority ? 0 : 1, item.priority || 0]} # list by priorty with nulls at the end
+        sorted = list.sort_by {|item| [item.priority ? 0 : 1, item.priority || 0]} # list by priorty with nulls at the end
+        sorted.each.with_index do |task,index|
+          puts "#{index+1} #{task}" 
+        end
       rescue
         puts "Unable to load your todo list."
       end
@@ -21,6 +24,21 @@ module Askew
       list << new_task
       list.save! #"#{Startup.config.todo_file}2)"
     end
+
+    desc "remove | rm TASK_#", "Remove a task"
+    def remove num
+      list = Todo::List.new Startup.config.todo_file
+      sorted = list.sort_by {|item| [item.priority ? 0 : 1, item.priority || 0]} # list by priorty with nulls at the end
+
+      index_to_rm = num.to_i - 1 
+      if yes? "[y,yes] Ok to remove task #{num}? : #{sorted[index_to_rm]} \n" 
+        puts "user confirmed"
+        list.delete sorted[index_to_rm]
+        list.save! 
+      end
+    end
+
+    map rm: :remove
 
     default_task :list
 
