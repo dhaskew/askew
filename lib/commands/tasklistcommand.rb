e8877428f1f104b6822cceff4075d1a41fddf987
David <david@aske.ws>
2017-05-24 06:34:27 -0400

    refactoring to enable better centralized output and list management

--- lib/commands/tasklistcommand.rb
+++ lib/commands/tasklistcommand.rb
@@ -8,18 +8,18 @@ module Askew
    
     desc "projects", "List all known projects"
     def projects
-      puts get_list.projects.sort
+      puts ListManager.get_list.projects.sort
     end
 
     desc "contexts", "List all known contexts"
     def contexts
-      puts get_list.contexts.sort
+      puts ListManager.get_list.contexts.sort
     end
 
     desc "all", "List all tasks"
     def all 
       begin
-        print_list get_sorted_list
+        Console.print_list ListManager.get_sorted_list
       rescue Exception => e
         puts "Unable to load your todo list."
         puts e
@@ -28,86 +28,17 @@ module Askew
 
     desc "search TERM", "List all tasks that contain TERM"
     def search term
-      print_list get_list.search term 
+      Console.print_list ListManager.get_list.search term 
     end
 
     desc "for_project X", "List all tasks connected to project X"
     def for_project proj
-      print_list get_list.for_project proj
+      Console.print_list ListManager.get_list.for_project proj
     end
 
     desc "for_context X", "List all tasks connected to context X"
     def for_context con
-      print_list get_list.for_context con
-    end
-
-
-    no_commands do   
-      
-      def get_sorted_list
-        list = get_list
-        sorted = list.sort_by {|key, value| [value.priority ? 0 : 1, value.priority || 0]} # list by priorty with nulls at the end
-      end 
-
-      def get_list
-        list = Askew::TaskList.new Startup.config.todo_file
-      end
-      
-      def print_list task_list=nil
-        return if task_list == nil
-        
-        ids = []
-        #collect only the line #'s
-        task_list.flatten.each_with_index {|item,index| ids << item if index.even?} 
-        #find the char count of the biggest line # 
-        max = ids.max.to_s.size
-
-        puts ""
-        puts "#{'#'*max} Pri Project  Context    Task"
-        puts "-"*150
-
-        temp_list = get_list
-        projects = temp_list.projects
-        contexts = temp_list.contexts 
-
-        task_list.each do |key,value|
-
-          num = "#{key.to_s.rjust(max,' ')}" 
-
-          pri = "   "
-          pri = "(#{value.priority})" if value.priority != nil
-
-          proj = value.projects.count == 1 ? value.projects[0] : "Multiple"
-          proj =  proj.ljust(8, ' ')
-
-          con = value.contexts.count == 1 ? value.contexts[0] : "Multiple"
-          con = con.ljust(8, ' ')
-
-          txt = value.text[0..70]
-          txt = txt.ljust(70, ' ')
-
-          tags = ""
-
-          truncate = "[...]"
-
-
-          value.tags.each {|key,value| tags = tags + key.to_s + ":" + value }
-
-          tags = tags[0..50] + " " + truncate if tags.size > 50
-
-          line = "#{num} #{pri} #{proj} #{con}   #{txt} #{tags}"
-
-          puts line
-
-        end
-
-        footer = "-"*150
-        puts footer
-        footer = "Task Count: #{task_list.count} | Projects: #{projects.join(' , ')} | Contexts: #{contexts.join(' , ')}"
-        puts footer
-        puts ""
-      end
-
+      Console.print_list ListManager.get_list.for_context con
     end
 
    default_task :all 
