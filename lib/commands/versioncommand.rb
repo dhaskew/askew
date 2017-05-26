114d8f5a9b88a3051d1814e0b4efd5b472676048
David <david@aske.ws>
2017-05-26 17:34:13 -0400

    version bump to 0.5.1

--- lib/commands/versioncommand.rb
+++ lib/commands/versioncommand.rb
@@ -4,7 +4,7 @@ module Askew
 
     VERSION_MAJOR    = '0'
     VERSION_MINOR    = '5'
-    VERSION_PATCH    = '0'
+    VERSION_PATCH    = '1'
 
     VERSION = VERSION_MAJOR + "." +  VERSION_MINOR + "." + VERSION_PATCH
     
