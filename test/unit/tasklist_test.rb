2369daf7aff0caaea693cdb04a5b277a54f72ab7
David <david@aske.ws>
2017-06-13 08:04:57 -0400

    .

---
+++ test/unit/tasklist_test.rb
@@ -0,0 +1,65 @@
+require_relative "../test_helper.rb"
+
+class TaskListTest < Minitest::Test
+
+  def test_tasklist_can_be_created_from_path
+    skip 
+  end
+
+  def test_tasklist_can_be_created_from_hash_of_tasks
+    #assert_equal true,false 
+    skip
+  end
+
+  def test_tasklist_has_list_of_projects
+    skip 
+  end
+
+  def test_tasklist_has_list_of_contexts
+    skip 
+  end
+
+  def test_tasklist_can_be_searched
+    skip 
+  end
+
+  def test_tasklist_can_be_sorted_by_priority
+    skip 
+  end
+
+  def test_tasklist_can_be_subsetted_by_context
+    skip 
+  end
+
+  def test_tasklist_can_be_subsetted_by_project
+    skip 
+  end
+
+  def test_tasklist_can_be_subsetted_by_done
+    skip
+  end
+  
+  def test_tasklist_can_be_subsetted_by_not_done
+    skip
+  end
+
+  def test_tasklist_can_purge_done_tasks_in_place
+    skip 
+  end
+
+  def test_tasklist_can_archive_done_tasks
+    skip
+    #tasklists with no path?
+  end
+
+  def test_tasklist_can_save_to_path
+    skip
+    #tasklists with no path?
+  end
+
+  def test_tasklist_can_be_backed_up
+    skip
+    #tasklists with no path? 
+  end
+
+end
