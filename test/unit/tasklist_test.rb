da55d61bb89f68de5eb3e9f604ee94257c6db408
David <david@aske.ws>
2017-06-19 06:32:20 -0400

    tasklist tests

--- test/unit/tasklist_test.rb
+++ test/unit/tasklist_test.rb
@@ -2,17 +2,25 @@ require_relative "../test_helper.rb"
 
 class TaskListTest < Minitest::Test
 
+  def setup
+    @t1 = Askew::Task.new("1st raw task line +proj1 @con1 +project2 @context2")
+    @t2 = Askew::Task.new("2nd raw task line +proj3 @con1 +project2 @context3")
+    @h1 = { 1 => @t1, 2 => @t2 }
+    @tl1 = Askew::TaskList.new @h1
+  end
+
   def test_tasklist_can_be_created_from_path
-    skip 
+    skip
+    #fakefs setup first? 
   end
 
   def test_tasklist_can_be_created_from_hash_of_tasks
-    #assert_equal true,false 
-    skip
+    assert_equal @tl1.size , 2 
   end
 
   def test_tasklist_has_list_of_projects
-    skip 
+    assert_equal @tl1.projects.size > 0, true 
+    #check contents of projects list
   end
 
   def test_tasklist_has_list_of_contexts
