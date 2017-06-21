41651148bf939cd4cce727c1b0db36b185ff3e4c
David <david@aske.ws>
2017-06-21 06:41:41 -0400

    default command tests

---
+++ test/functional/default_command_test.rb
@@ -0,0 +1,30 @@
+require_relative "../test_helper.rb"
+
+class DefaultCommandTest < Minitest::Test
+
+  # Ensure the default command is "askew task list all",
+  # regardless of wether it is executed in whole or in part
+  def test_default_command_is_task_list
+    out, err = capture_io {
+      Askew::CLI.start %w{ }
+    }
+
+    out2, err2 = capture_io {
+      Askew::CLI.start %w{ task }
+    }
+
+    out3, err3 = capture_io {
+      Askew::CLI.start %w{ task list }
+    }
+
+    out4, err4 = capture_io {
+      Askew::CLI.start %w{ task list all }
+    }
+
+    refute_nil out
+    assert_equal out, out2
+    assert_equal out, out3
+    assert_equal out, out4
+  end
+
+end
