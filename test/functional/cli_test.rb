fa4cf7b6cefd993b28cc18118da6b030de11666a
David <david@aske.ws>
2017-06-21 06:19:07 -0400

    basic cli tests

--- test/functional/cli_test.rb
+++ test/functional/cli_test.rb
@@ -9,4 +9,30 @@ class CliTest < Minitest::Test
     assert out == Askew::VersionCommand::VERSION + "\n"
   end
 
+  def test_cli_version_major_command
+    out, err = capture_io {
+      Askew::CLI.start %w{ version major }
+    }
+    assert out == Askew::VersionCommand::VERSION_MAJOR + "\n"
+  end
+  
+
+  def test_cli_version_minor_command
+    out, err = capture_io {
+      Askew::CLI.start %w{ version minor }
+    }
+    assert out == Askew::VersionCommand::VERSION_MINOR + "\n"
+  end
+
+  def test_cli_version_patch_command
+    out, err = capture_io {
+      Askew::CLI.start %w{ version patch }
+    }
+    assert out == Askew::VersionCommand::VERSION_PATCH + "\n"
+  end
+  
+  def test_cli_help_option
+    skip 
+  end
+
 end
