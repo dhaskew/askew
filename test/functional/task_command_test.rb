cabd6dc7d1de16a71516b4fbc4054b07117029a5
David <david@aske.ws>
2017-06-22 18:06:19 -0400

    added skipped test stubs for tasks

--- test/functional/task_command_test.rb
+++ test/functional/task_command_test.rb
@@ -1,7 +1,61 @@
 require_relative "../test_helper.rb"
 
 class TaskCommandTest < Minitest::Test
-  def test_task_command
+
+  def test_task_can_be_added
     skip
   end
+
+  def test_task_can_be_removed
+    skip
+  end
+
+  def test_task_can_be_shown
+    skip
+  end
+
+  def test_task_can_be_done
+    skip
+  end
+
+  def test_task_can_be_undone
+    skip
+  end
+
+  def test_task_can_be_updated
+    skip
+  end
+
+  def test_task_can_be_prioritized
+    skip
+  end
+
+  def test_task_can_be_deprioritized
+    skip
+  end
+
+  def test_task_can_have_priority_removed
+    skip
+  end
+
+  def test_task_can_be_tagged
+    skip
+  end
+
+  def test_task_can_be_added_to_project
+    skip
+  end
+
+  def test_task_can_be_contextualized
+    skip
+  end
+
+  def test_task_can_be_navigated_to
+    skip
+  end
+
+  def test_task_can_be_archived
+    skip
+  end
+
 end
