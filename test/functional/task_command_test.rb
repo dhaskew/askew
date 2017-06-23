006cc21b583bbeabba04dd9317404361553e4f27
David <david@aske.ws>
2017-06-23 16:46:12 -0400

    task_commmand_test tests are broken

--- test/functional/task_command_test.rb
+++ test/functional/task_command_test.rb
@@ -2,8 +2,40 @@ require_relative "../test_helper.rb"
 
 class TaskCommandTest < Minitest::Test
 
+  ORIGINAL_CONFIG_FILE_CONSTANT_VALUE = Askew::Config::DEFAULT_CONFIG_FILE
+  ORIGINAL_CONFIG_FILE_CONSTANT_NAME = "DEFAULT_CONFIG_FILE"
+ 
+  TEST_CONFIG_FILE = "test/inputs/test.config"
+
+  def setup
+    # https://stackoverflow.com/questions/3375360/how-to-redefine-a-ruby-constant-without-warning
+    Askew::Config.send(:remove_const, ORIGINAL_CONFIG_FILE_CONSTANT_NAME)
+    Askew::Config.const_set(ORIGINAL_CONFIG_FILE_CONSTANT_NAME, TEST_CONFIG_FILE)
+  end
+
+  def teardown
+    Askew::Config.send(:remove_const, ORIGINAL_CONFIG_FILE_CONSTANT_NAME)
+    Askew::Config.const_set(ORIGINAL_CONFIG_FILE_CONSTANT_NAME, ORIGINAL_CONFIG_FILE_CONSTANT_VALUE)
+  end
+
   def test_task_can_be_added
-    skip
+   begin 
+    config_path = File.expand_path(TEST_CONFIG_FILE)
+    config = Askew::Config.new config_path
+    task_file = Askew::Config.new(TEST_CONFIG_FILE).todo_file
+    out, err = capture_io {
+#      FakeFS.with_fresh do
+#        FakeFS::FileSystem.clone(File.expand_path(config_path))
+#        FakeFS::FileSystem.clone(File.expand_path(task_file))
+        Askew::CLI.start %w{ version }
+     # end 
+    }
+   rescue
+     puts "exception happend"
+   end
+    puts "foobar"
+    puts out
+    assert out == Askew::VersionCommand::VERSION + "\n"
   end
 
   def test_task_can_be_removed
